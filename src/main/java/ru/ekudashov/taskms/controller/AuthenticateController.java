a4ff6bc9ff3b0586ae69757897185da384261a79
Evgeniy Kudashov <kudashovea@gmail.com>
2023-12-09 22:48:09 +0300

    added @validated annotation to @RequestBody parameters

--- src/main/java/ru/ekudashov/taskms/controller/AuthenticateController.java
+++ src/main/java/ru/ekudashov/taskms/controller/AuthenticateController.java
@@ -2,6 +2,7 @@ package ru.ekudashov.taskms.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class AuthenticateController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(path = "/authenticate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public AccessTokenResponseDto authenticate(@RequestBody EmailPasswordRequestDto requestDto) {
+    public AccessTokenResponseDto authenticate(@Validated @RequestBody EmailPasswordRequestDto requestDto) {
 
         return authenticationService.authenticate(requestDto);
     }
--- src/main/java/ru/ekudashov/taskms/controller/TaskController.java
+++ src/main/java/ru/ekudashov/taskms/controller/TaskController.java
@@ -1,18 +1,19 @@
 package ru.ekudashov.taskms.controller;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.ekudashov.taskms.dto.PageDto;
 import ru.ekudashov.taskms.dto.TaskRequestDto;
 import ru.ekudashov.taskms.dto.TaskResponseDto;
 import ru.ekudashov.taskms.service.TaskService;
+
 
 @RestController
 @RequestMapping(path = "/api/1/tasks")
@@ -22,32 +23,33 @@ public final class TaskController {
     private final TaskService taskService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TaskResponseDto createTask(@RequestBody TaskRequestDto taskRequestDto) {
+    public TaskResponseDto createTask(@Validated @RequestBody TaskRequestDto taskRequestDto) {
 
         return taskService.createTask(taskRequestDto);
     }
 
     @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public TaskResponseDto updateTask(@PathVariable long id, TaskRequestDto taskRequestDto) {
+    public TaskResponseDto updateTask(@Positive @PathVariable long id,
+                                      @Validated @RequestBody TaskRequestDto taskRequestDto) {
 
         return taskService.updateTask(id, taskRequestDto);
     }
 
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(code = HttpStatus.OK)
-    public void deleteTask(@PathVariable @NonNull Long id) {
+    public void deleteTask(@PathVariable @Positive Long id) {
 
         taskService.deleteTaskById(id);
     }
 
     @GetMapping(path = "/{id}")
-    public TaskResponseDto getTaskById(@PathVariable @NonNull Long id) {
+    public TaskResponseDto getTaskById(@PathVariable @Positive Long id) {
 
         return taskService.findTaskById(id);
     }
 
     @PostMapping(path = "/search")
-    public PageDto<TaskResponseDto> searchTasks(@RequestBody TaskRequestDto taskRequestDto, @PageableDefault Pageable pageable) {
+    public PageDto<TaskResponseDto> searchTasks(@Validated @RequestBody TaskRequestDto taskRequestDto, @PageableDefault Pageable pageable) {
 
         return taskService.findTasks(taskRequestDto, pageable);
     }
