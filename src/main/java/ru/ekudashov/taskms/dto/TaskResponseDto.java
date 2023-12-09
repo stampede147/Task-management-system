52f0153b94426f527bf57361688170c04ecf758b
Evgeniy Kudashov <kudashovea@gmail.com>
2023-12-09 23:57:01 +0300

    added fields to response

--- src/main/java/ru/ekudashov/taskms/dto/TaskResponseDto.java
+++ src/main/java/ru/ekudashov/taskms/dto/TaskResponseDto.java
@@ -1,4 +1,31 @@
 package ru.ekudashov.taskms.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+import ru.ekudashov.taskms.model.TaskPriority;
+import ru.ekudashov.taskms.model.TaskStatus;
+
+@Data
 public class TaskResponseDto {
+
+    @NotEmpty
+    public String title;
+
+    @NotEmpty
+    public String description;
+
+    @NotNull
+    public TaskStatus status;
+
+    @NotNull
+    public TaskPriority priority;
+
+    @Positive
+    public long creator;
+
+    @Positive
+    public long performer;
+
 }
